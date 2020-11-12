653271a2620b9633981961e364cd8f2e5a7c24b6
gitirana <tlgc@poli.br>
2020-11-11 22:18:20 -0300

    first commit

---
+++ backend/src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import routes from './routes';
+
+import 'reflect-metadata';
+import './database';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333, () => {
+  // eslint-disable-next-line no-console
+  console.log('🚀 Server started on port 3333');
+});
