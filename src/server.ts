866886bc4b1d59d88ea6b237428eaf6d107a3098
gitirana <tlgc@poli.br>
2020-11-14 00:24:26 -0300

    exception handling

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,35 @@
-import express from 'express';
-import routes from './routes';
+/* eslint-disable no-console */
+import express, { NextFunction, Request, Response } from 'express';
 
+import 'express-async-errors';
 import 'reflect-metadata';
 import './database';
+
+import uploadConfig from './config/upload';
+import AppError from './errors/AppError';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3333, () => {
   // eslint-disable-next-line no-console
