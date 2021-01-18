8388b34dd5c6790e26632a55b6beffd6a09f27b0
thegitirana <thayna@talentacademy.com.br>
2021-01-18 17:27:15 -0300

    updating

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -6,7 +6,7 @@ const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
 export default {
   tmpFolder,
-  uploadsFolder: path.resolve(tmpFolder),
+  uploadsFolder: path.resolve(tmpFolder, 'uploads'),
 
   storage: multer.diskStorage({
     destination: tmpFolder,
